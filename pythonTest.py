1d591ba2aa79a685a59a6ea2e3615c89ab821a5b
Juan Camilo Martinez Morales <xogost@linux-gdxc.site>
2012-05-18 00:46:56 -0500

    second commit

--- pythonTest.py
+++ pythonTest.py
@@ -10,8 +10,11 @@ class test_basic(unittest.TestCase):
 	def testNumeroDos(self):
 		classNumero = returnNumeros(None)
 		assert classNumero.numeroDos == 2	
-						
+	
+	def testNumeroTres(self):
+		classNumero = returnNumeros(None)
+		assert classNumero.returnNumeroTres() == 3
+
 if __name__=='__main__':
 	unittest.main()
-	print __name__
 		
