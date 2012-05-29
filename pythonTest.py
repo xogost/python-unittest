4d9abcf97bd729d5449eeb4409102b9a676c422e
Juan Camilo Martinez Morales <xogost@linux-gdxc.site>
2012-05-29 00:31:47 -0500

    Agreda la suma para los numeros del 1-10

--- pythonTest.py
+++ pythonTest.py
@@ -3,9 +3,73 @@ import unittest
 from returnNumeros import Romannumerals
 
 class test_basic(unittest.TestCase):
-	def testresultforSum(self):
+	def testNumeroDos(self):
 		classNumero = Romannumerals()
-		assert classNumero.Sum('I','I') == 1
+		assert classNumero.Sum('I','I') == 2
+
+	def testNumeroTres(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('I','II') == 3
+
+	def testNumeroTresInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('II','I') == 3
+
+	def testNumeroCuatro(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('III','I') == 4
+
+	def testNumeroCuatroInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('I','III') == 4
+
+	def testNumeroCinco(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('IV','I') == 5
+
+	def testNumeroCincoInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('I','IV') == 5	
+
+	def testNumeroSeis(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('I','V') == 6
+
+	def testNumeroSeisInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('V','I') == 6
+
+	def testNumeroSiete(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('II','V') == 7
+
+	def testNumeroSieteInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('V','II') == 7
+
+	def testNumeroOcho(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('III','V') == 8
+
+	def testNumeroOchoInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('V','III') == 8
+
+	def testNumeroNueve(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('IV','V') == 9
+
+	def testNumeroNueveInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('V','IV') == 9
+
+	def testNumeroDiez(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('I','IX') == 10
+
+	def testNumeroDiezInverso(self):
+		classNumero = Romannumerals()
+		assert classNumero.Sum('IX','I') == 10 
 
 if __name__=='__main__':
 	unittest.main()
--- returnNumeros.py
+++ returnNumeros.py
@@ -1,12 +1,41 @@
 #Calculadora de numeros romanos
 #Juan Camilo Martinez
-#Para Codigdojo Bogota
+#Para Codindojo Bogota
 
 class Romannumerals(object):
 	"""Roman numerals class"""
 	def __init__(self):
 		super(Romannumerals, self).__init__()
 		self.numbers = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+		_numberOne = 0
+		_numberTwo = 0
+	
 	def Sum(self,numberOne,numberTwo):
-		return(self.numbers['I'])
+		self._numberOne = self.convertToNumber(numberOne)
+		self._numberTwo = self.convertToNumber(numberTwo)
+		result = self._numberOne + self._numberTwo
+		return(result)
+	
+	def convertToNumber(self, number):
+		_number = str(number)		
+
+		one = 0
+
+		for x in xrange(0,len(_number)):
+			if len(_number) == 2 and _number != 'II':
+				if _number[x+1] == 'V' and _number[x] == 'I':
+					_number = self.numbers['V'] -1 
+					break
+				elif _number[x+1] == 'X' and _number[x] == 'I':
+					  _number = self.numbers['X'] - 1
+					  break
+			elif _number[x] == 'V':
+				_number = self.numbers['V']
+			elif _number[x] == 'I':
+				one = one + 1
+
+		if one != 0:
+			_number = one
+
+		return int(_number)
 		
